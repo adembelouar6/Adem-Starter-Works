4f07ca85e7a830824602a11d3916a9627f180d80
adembelouar6 <adembelouar6@gmail.com>
2025-09-23 22:29:10 +0100

    Create learn_ccp_fourth_code.cpp
    New code ladies 
    Logical operations / some if and else tries

---
+++ learn_cpp_fourth_code/learn_ccp_fourth_code.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+// MY PC IS BACK LADIES I MISSED THIS SHIT
+int main()
+{
+    // WE ARE JUST DOING AN EXERCISE ON COMPARISON AND LOGICAL OPERATION
+    // Variables part
+    int age;
+    double energy;
+    int level;
+    // Asking part
+    cout<<"Daddy pls Enter your age | energy | level one by one :"<<endl;
+    // Input part
+    cin>>age;
+    cin>>energy;
+    cin>>level;
+    // now idk what im doing never learned if else
+    if (age>=18) 
+        cout<<"enter daddy ur an adult"<<endl;
+        else cout<<"what a minor"<<endl;
+    if (level<=5 || energy<=20 )
+    cout<<"go to the kitchen instead"<<endl;
+    else cout<<"pls carry me daddy"<<endl;
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
